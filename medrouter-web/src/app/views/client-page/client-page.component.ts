63b629da97b0b23dc14c7b04997aeb902ebda491
Nervado <evandroabreudeoliveira@gmail.com>
2020-04-20 01:25:47 -0300

    add: page client-area

--- medrouter-web/src/app/views/client-page/client-page.component.ts
+++ medrouter-web/src/app/views/client-page/client-page.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import {
+  faPowerOff,
+  faHome,
+  faUser,
+  faFileMedical,
+  faFileInvoiceDollar,
+  faBookMedical,
+  faCalendarCheck,
+  faPoll,
+} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-client-page',
-  templateUrl: './client-page.component.html',
-  styleUrls: ['./client-page.component.scss']
+  selector: "app-client-page",
+  templateUrl: "./client-page.component.html",
+  styleUrls: ["./client-page.component.scss"],
 })
 export class ClientPageComponent implements OnInit {
+  faPowerOff = faPowerOff;
 
-  constructor() { }
+  faHome = faHome;
+  faUser = faUser;
+  faFileMedical = faFileMedical;
+  faFileInvoiceDollar = faFileInvoiceDollar;
+  faBookMedical = faBookMedical;
+  faCalendarCheck = faCalendarCheck;
+  faPoll = faPoll;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
