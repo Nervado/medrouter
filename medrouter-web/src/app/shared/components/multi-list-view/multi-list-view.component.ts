3a311709143cd5255c6e7710e837c04e6330cf43
Nervado <evandroabreudeoliveira@gmail.com>
2020-06-04 03:45:23 -0300

    add: owners-doctor-dashboard actions

---
+++ medrouter-web/src/app/shared/components/multi-list-view/multi-list-view.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, AfterContentInit } from "@angular/core";
+import { Colors } from "src/app/messages/toast/enums/colors";
+import { capitalizeAndRemoveUnderscores } from "src/app/utils/capitalizeAndRemoveUnderscore";
+import { faAngleUp, faAngleDown } from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: "app-multi-list-view",
+  templateUrl: "./multi-list-view.component.html",
+  styleUrls: ["./multi-list-view.component.scss"],
+})
+export class MultiListViewComponent implements OnInit {
+  @Input() tags: string[] = [];
+  @Input() w: string = "150px";
+  @Input() h: string = "30px";
+  @Input() mainColor: Colors = Colors.BASE;
+
+  faAngleUp = faAngleUp;
+  faAngleDown = faAngleDown;
+
+  point: number = 0;
+  length: number = 0;
+
+  constructor() {}
+  ngOnInit(): void {
+    this.length = this.tags?.length;
+    this.tags = this.tags?.map((value) =>
+      capitalizeAndRemoveUnderscores(value)
+    );
+  }
+
+  up() {
+    if (this.point < this.length) this.point += 1;
+  }
+
+  down() {
+    if (this.point > 0) this.point -= 1;
+  }
+}
