275cceefc6374d6e2a46c731f6b68afb1e80ec4d
Nervado <evandroabreudeoliveira@gmail.com>
2020-09-28 12:16:33 -0300

    add media querys

--- medrouter-web/src/app/shared/components/tool-bar/tool-bar.component.ts
+++ medrouter-web/src/app/shared/components/tool-bar/tool-bar.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  HostListener,
+} from "@angular/core";
 
 import {
   faPlus,
@@ -16,12 +24,6 @@ import {
   faClock,
   faCalendarDay,
   faSignOutAlt,
-  faUsersCog,
-  faUserMd,
-  faConciergeBell,
-  faUserCog,
-  faFlask,
-  faHeartbeat,
 } from "@fortawesome/free-solid-svg-icons";
 
 import {} from "@fortawesome/free-solid-svg-icons";
@@ -36,7 +38,7 @@ import { Buttons } from "./theme/buttons.theme";
   templateUrl: "./tool-bar.component.html",
   styleUrls: ["./tool-bar.component.scss"],
 })
-export class ToolBarComponent implements OnInit {
+export class ToolBarComponent implements OnInit, AfterViewInit {
   faPowerOff = faPowerOff;
   faHome = faHome;
   faUser = faUser;
@@ -64,7 +66,50 @@ export class ToolBarComponent implements OnInit {
 
   menuIcons: Array<Icon> = [];
 
+  showNavigation: boolean = true;
+
+  height = "100%";
+
+  minHeight;
+
+  innerWidth: number;
+
+  @ViewChild("toolBarMenu") elementView: ElementRef;
+
   constructor() {}
+
+  ngAfterViewInit() {
+    this.height = "100%";
+
+    this.showNavigation = true;
+
+    this.innerWidth = window.innerWidth;
+
+    if (this.innerWidth > 640) {
+      this.minHeight = "80px";
+    } else {
+      this.minHeight = "50px";
+    }
+  }
+
+  @HostListener("window:resize", ["$event"])
+  onResize(event) {
+    this.innerWidth = window.innerWidth;
+
+    if (this.innerWidth > 640) {
+      this.minHeight = "80px";
+    } else {
+      this.minHeight = "50px";
+    }
+
+    this.toogle();
+  }
+
+  toogle() {
+    this.showNavigation = !this.showNavigation;
+
+    this.height = this.showNavigation ? "100%" : this.minHeight;
+  }
 
   ngOnInit(): void {
     this.menuIcons = Buttons[this.role];
